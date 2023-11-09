d913e1c8605ec5af78166f80870c5e851dd2100f
Sanni Moisio <sannimoi@FVFH9091Q05Q.local>
2023-11-09 15:33:36 +0200

    Fixing column order in create_learning2014.R script

--- create_learning2014.R
+++ create_learning2014.R
@@ -34,15 +34,15 @@ deep_columns <- select(data, one_of(deep_questions))
 # and creating column 'deep' by averaging
 data2$deep <- rowMeans(deep_columns)
 
-# selecting the columns related to surface learning 
-surface_columns <- select(data, one_of(surface_questions))
-# and creating column 'surf' by averaging
-data2$surf <- rowMeans(surface_columns)
-
 # selecting the columns related to strategic learning 
 strategic_columns <- select(data, one_of(strategic_questions))
 # and creating column 'stra' by averaging
 data2$stra <- rowMeans(strategic_columns)
+
+# selecting the columns related to surface learning 
+surface_columns <- select(data, one_of(surface_questions))
+# and creating column 'surf' by averaging
+data2$surf <- rowMeans(surface_columns)
 
 # reordering the table
 data2 <- data2[,c(1:3,5:7,4)]
@@ -65,20 +65,20 @@ dim(data)
 # 166 rows, 7 columns
 head(data)
 # gender age attitude     deep     surf  stra points
-# 1      F  53       37 3.583333 2.583333 3.375     25
-# 2      M  55       31 2.916667 3.166667 2.750     12
-# 3      F  49       25 3.500000 2.250000 3.625     24
-# 4      M  53       35 3.500000 2.250000 3.125     10
-# 5      M  49       37 3.666667 2.833333 3.625     22
-# 6      F  38       38 4.750000 2.416667 3.625     21
+# 1      F  53       37 3.583333 3.375 2.583333     25
+# 2      M  55       31 2.916667 2.750 3.166667     12
+# 3      F  49       25 3.500000 3.625 2.250000     24
+# 4      M  53       35 3.500000 3.125 2.250000     10
+# 5      M  49       37 3.666667 3.625 2.833333     22
+# 6      F  38       38 4.750000 3.625 2.416667     21
 str(data)
 # 'data.frame':	166 obs. of  7 variables:
 # $ gender  : chr  "F" "M" "F" "M" ...
 # $ age     : num  53 55 49 53 49 38 50 37 37 42 ...
 # $ attitude: num  37 31 25 35 37 38 35 29 38 21 ...
 # $ deep    : num  3.58 2.92 3.5 3.5 3.67 ...
-# $ surf    : num  2.58 3.17 2.25 2.25 2.83 ...
 # $ stra    : num  3.38 2.75 3.62 3.12 3.62 ...
+# $ surf    : num  2.58 3.17 2.25 2.25 2.83 ...
 # $ points  : num  25 12 24 10 22 21 21 31 24 26 ...
 
 # structure looks correct
